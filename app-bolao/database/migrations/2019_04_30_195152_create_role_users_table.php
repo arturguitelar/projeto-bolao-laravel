c721475aa7e3006e277b6685fbfbc6a4b9bc4f2b
Artur Killdragon <artur.guitelar@gmail.com>
2019-04-30 16:58:28 -0300

    Add: Modelo de relacionamento entre função e usuário.

---
+++ app-bolao/database/migrations/2019_04_30_195152_create_role_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoleUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            // only positive numbers
+            $table->integer('user_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+
+            // relações
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropForeign('role_user_role_id_foreign');
+            $table->dropForeign('role_user_permission_id_foreign');
+        });
+
+        Schema::dropIfExists('role_user');
+    }
+}
