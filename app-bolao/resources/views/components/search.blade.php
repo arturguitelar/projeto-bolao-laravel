7f2074d0fd876e78b1f9956bdd0ba2de28352650
Artur Killdragon <artur.guitelar@gmail.com>
2019-06-06 20:28:57 -0300

    Utilização de ACL na view de criação de usuários.

--- app-bolao/resources/views/components/search.blade.php
+++ app-bolao/resources/views/components/search.blade.php
@@ -1,7 +1,10 @@
 <form class="form-inline" method="GET" action="{{route($routeName.'.index')}}">
+    
+    @can('create-user')
     <div class="form-group mb-2">
         <a href="{{route($routeName.'.create')}}">@lang('bolao.add')</a>
     </div>
+    @endcan
 
     <div class="form-group mx-sm-3 mb-2">
         <input type="search" class="form-control" name="search" placeholder="@lang('bolao.search')" value="{{$search}}">
--- app-bolao/routes/web.php
+++ app-bolao/routes/web.php
@@ -14,18 +14,18 @@
 Route::get('lang', function () {
     $lang = session('lang', 'pt-br');
     if($lang == 'pt-br'){
-      $lang = "en";
-
+        $lang = "en";
+        
     }else{
-      $lang = "pt-br";
+        $lang = "pt-br";
     }
     session(['lang' => $lang]);
     return redirect()->back();
-
+    
 })->name('lang');
 
 Route::get('/', function () {
-
+    
     return view('welcome');
 });
 
@@ -34,7 +34,25 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::prefix('admin')->middleware('auth')->namespace('Admin')->group(function () {
+    
+    /* Users */
     Route::resource('/users', 'UserController');
+    
+    Route::get('/users', 'UserController@index')
+        ->name('users.index')
+        ->middleware('can:list-users');
+
+    Route::get('/users/create', 'UserController@create')
+        ->name('users.create')
+        ->middleware('can:create-user');
+    
+    Route::post('/users', 'UserController@store')
+        ->name('users.store')
+        ->middleware('can:create-user');
+    
+    /* Permissions */
     Route::resource('/permissions', 'PermissionController');
+
+    /* Roles */
     Route::resource('/roles', 'RoleController');
 });
